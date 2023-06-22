e2c44f9dd4b1deba1c331293be7ed3e43044b60c
Eliezer-Moo <fuuzze@gmail.com>
2023-06-22 17:33:14 -0600

    relacion de tablas categorias y tareas

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -8,10 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tarea extends Model
 {
     use HasFactory;
-    protected $table= 'categories';
+    protected $table= 'tareas';
     protected $fillable = ['titulo','descripcion','estado','prioridad','set_at','finish_at'];
 
     public function categorias(){
-        return $this->belongsTo(Category::class, 'id');
+        return $this->belongsTo(Category::class, 'category_id');
     }
 }
